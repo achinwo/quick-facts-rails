c17c2f04f30121146aa22abee79fbb0c4279f8e0
Anthony Chinwo <anthony.chinwo@gmail.com>
2016-01-24 15:36:35 +0000

    recompile assets

--- gcloud/configure.sh
+++ gcloud/configure.sh
@@ -61,6 +61,7 @@ chown -R railsapp:railsapp "$DIR"
 sudo -u railsapp -H bundle install --path "$DIR"
 sudo -u railsapp -H bundle exec rake db:create RAILS_ENV="production"
 sudo -u railsapp -H bundle exec rake db:migrate RAILS_ENV="production"
+sudo -u railsapp -H bundle exec rake assets:precompile RAILS_ENV="production"
 
 cat gcloud/default-nginx > /etc/nginx/sites-available/default
 systemctl restart nginx.service
