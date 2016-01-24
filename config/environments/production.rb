b96ff5bf02dc96ba844f187e953ac0f0c5de6f6d
Anthony Chinwo <anthony.chinwo@gmail.com>
2016-01-24 18:19:10 +0000

    updated host to quickfacts.co

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -77,7 +77,7 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { :host => "http://130.211.80.156" }
+  config.action_mailer.default_url_options = { :host => "quickfacts.co" }
 
   # I recommend using this line to show error
   config.action_mailer.raise_delivery_errors = true
@@ -88,7 +88,7 @@ Rails.application.configure do
     :authentication => :plain,
     :user_name      => 'SENDGRID_USERNAME',
     :password       => 'SENDGRID_PASSWORD',
-    :domain         => 'aetoslabs.com',
+    :domain         => 'quickfacts.co',
     :enable_starttls_auto => true
 }
 end
